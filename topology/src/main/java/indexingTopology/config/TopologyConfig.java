dbcd311517b7a02a0f973b838ef01a9b8c231478
Li Wang <wangli1426@gmail.com>
2017-07-06 10:15:34 +0800

    removed unnecessary code

--- topology/src/main/java/indexingTopology/config/TopologyConfig.java
+++ topology/src/main/java/indexingTopology/config/TopologyConfig.java
@@ -8,9 +8,9 @@ import java.io.Serializable;
 public class TopologyConfig implements Serializable {
     public final double REBUILD_TEMPLATE_THRESHOLD = 10.0;
 
-//    public static final String HDFS_HOST = "hdfs://192.168.0.237:54310/";
+    public static final String HDFS_HOST = "hdfs://192.168.0.237:54310/";
 
-    public final String HDFS_HOST = "hdfs://10.21.25.10:54310/";
+//    public final String HDFS_HOST = "hdfs://10.21.25.10:54310/";
 //    public static final String HDFS_HOST = "hdfs://10.21.25.13:54310/";
 
     /**
@@ -22,8 +22,6 @@ public class TopologyConfig implements Serializable {
     public final String HDFS_META_LOG_PATH = "hdfs://localhost:9000/user/john/metaLog.txt";
     public final String HDFS_HOST_LOCAL = "hdfs://localhost:9000/";
 
-    public final int NUMBER_TUPLES_OF_A_CHUNK = 600000;
-
     public final int CACHE_SIZE = 200;
 
     public final int TASK_QUEUE_CAPACITY = 10000;
@@ -34,11 +32,11 @@ public class TopologyConfig implements Serializable {
 
     public final double LOAD_BALANCE_THRESHOLD = 0.2;
 
-    public boolean HDFSFlag = false;
+    public boolean HDFSFlag = true;
 
     public boolean HybridStorage = false;
 
-    public boolean HdfsTaskLocality = false;
+    public boolean HdfsTaskLocality = true;
 
     public boolean ChunkOrientedCaching = true;
 
@@ -49,7 +47,7 @@ public class TopologyConfig implements Serializable {
 
 //    public static String dataDir = "./";
 
-    public String dataFileDir = "/home/robert/data/t";
+    public String dataFileDir = "/home/robert/data";
 //    public static String dataFileDir = "/home/lzj/dataset/20150430_processed.txt";
 //    public static String dataFileDir = "/home/acelzj/Downloads/DPI/20150430_processed.txt";
 //    public static String dataFileDir = "/home/acelzj/Downloads/DPI/20150430.txt";
@@ -92,7 +90,7 @@ public class TopologyConfig implements Serializable {
     public final boolean TASK_QUEUE_MODEL = false;
 
     // parallelism configuration
-    public int CHUNK_SCANNER_PER_NODE = 2;
+    public int CHUNK_SCANNER_PER_NODE = 4;
 
     public int INSERTION_SERVER_PER_NODE = 2;
 }
