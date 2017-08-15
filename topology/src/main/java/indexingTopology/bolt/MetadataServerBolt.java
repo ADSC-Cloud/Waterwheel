c1480c8eb976f70bc800218f99f95824b1c9d0b2
z-linH <455035961@qq.com>
2017-08-15 14:26:36 +0800

    new commit

--- topology/src/main/java/indexingTopology/bolt/MetadataServerBolt.java
+++ topology/src/main/java/indexingTopology/bolt/MetadataServerBolt.java
@@ -136,22 +136,19 @@ public class MetadataServerBolt<Key extends Number> extends BaseRichBolt {
 //        }
 
         systemState = new SystemState();
-        systemStateQueryServer = new Server(20000, SystemStateQueryHandle.class, new Class[]{SystemState.class}, systemState);
-
         staticsRequestSendingThread = new Thread(new StatisticsRequestSendingRunnable());
         staticsRequestSendingThread.start();
 
+
 //        createMetadataSendingThread();
-
-        systemState = new SystemState();
-        System.out.println("dataChunkDir:"+config.dataChunkDir);
-        systemState.hashMap = new HashMap<>();
-        systemState.hashMap.put("dataChunkDir",config.dataChunkDir);
-        systemState.hashMap.put("metadataDir",config.metadataDir);
+//        System.out.println("aaaaaaaaaaaa:"+config.dataChunkDir);
+        String a = "aa";
+//        systemState.setHashMap("11",a);
+//        systemState.setHashMap("22","bb");
+        systemState.setTreeMap("dataChunkDir",config.dataChunkDir);
+        systemState.setTreeMap("metadataDir",config.metadataDir);
         systemStateQueryServer = new Server(20000, SystemStateQueryHandle.class, new Class[]{SystemState.class}, systemState);
-
         systemStateQueryServer.startDaemon();
-
     }
 
     private void createMetadataSendingThread() {
@@ -433,7 +430,7 @@ public class MetadataServerBolt<Key extends Number> extends BaseRichBolt {
 //
 //        int partitionId = 0;
 
-//        long tmpWorkload = 0;
+    //        long tmpWorkload = 0;
 //
 //        List<Long> workLoads = histogram.histogramToList();
 //
@@ -465,9 +462,9 @@ public class MetadataServerBolt<Key extends Number> extends BaseRichBolt {
 
         @Override
         public void run() {
-            final int sleepTimeInSecond = 10;
+            final int sleepTimeInSecond = 5;
 //            while (true) {
-            systemState.lastThroughput = new double[6];
+            systemState.setLastThroughput(new double[6]);
             int i = 0;//throughput计数器.
             while (!Thread.currentThread().isInterrupted()) {
                 try {
@@ -482,24 +479,40 @@ public class MetadataServerBolt<Key extends Number> extends BaseRichBolt {
                 for(Long count: counts) {
                     sum += count;
                 }
-                systemState.throughout =  sum / (double)sleepTimeInSecond;
-                systemState.lastThroughput[i++] = systemState.throughout;
-                System.out.println("i: "+i+" "+systemState.lastThroughput[i-1]);
-                if(i >= systemState.lastThroughput.length){
-                    for(int j = 0;j< systemState.lastThroughput.length-1;j++){
-                        systemState.lastThroughput[j] = systemState.lastThroughput[j+1];
-                    }
-                    systemState.lastThroughput[systemState.lastThroughput.length-1] = systemState.throughout;
-                    i--;
+                systemState.setThroughout(sum / (double)sleepTimeInSecond);
+                systemState.setCpuRatio(60);
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!throughput:  "+systemState.getThroughput()+"----------------------");
+                int throughputSite = systemState.lastThroughput.length-1;
+                while(throughputSite > 0){
+                    systemState.lastThroughput[throughputSite] = systemState.lastThroughput[throughputSite-1];
+                    throughputSite--;
                 }
+                systemState.lastThroughput[0] = systemState.getThroughput();
+//                if(i >= systemState.lastThroughput.length){
+//                    for(int j = 0;j< systemState.lastThroughput.length-1;j++){
+//                        systemState.lastThroughput[j] = systemState.lastThroughput[j+1];
+////                            systemState.lastThroughput[j] = j+1;
+//                    }
+//                    systemState.lastThroughput[systemState.lastThroughput.length-1] = systemState.getThroughput();
+////                    systemState.lastThroughput[systemState.lastThroughput.length-1] = 1;
+//                }
+//                else{
+//                    if(systemState.getThroughput() != 0){
+//                        systemState.lastThroughput[i++] = systemState.getThroughput();
+//                        System.out.println("i: "+i+" "+systemState.lastThroughput[i-1]);
+//                    }
+////                    systemState.lastThroughput[i++] = i+1;
+//                }
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!lastThroughput:  "+systemState.lastThroughput[0]+" "+systemState.lastThroughput[1]+" "+systemState.lastThroughput[2]+" "+systemState.lastThroughput[3]+" "+systemState.lastThroughput[4]+" "+systemState.lastThroughput[5]+"----------------------");
+
 //                System.out.println("statics request has been sent!!!");
                 System.out.println(String.format("Overall Throughput: %f tuple / second", sum / (double)sleepTimeInSecond));
 
                 histogram.clear();
 
 //                if (repartitionEnabled) {
-                    collector.emit(Streams.StaticsRequestStream,
-                            new Values("Statics Request"));
+                collector.emit(Streams.StaticsRequestStream,
+                        new Values("Statics Request"));
 //                }
             }
         }
