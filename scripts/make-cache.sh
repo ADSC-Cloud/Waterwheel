afea981605e88d6baced4f18a10ed1333b514249
Li Wang <wangli1426@gmail.com>
2017-11-23 22:08:02 +0800

    using [[ xxx ]] in string comparison

--- scripts/make-cache.sh
+++ scripts/make-cache.sh
@@ -7,7 +7,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 echo "checking hadoop-2.8.1.tar.gz ..."
-if [ -s hadoop-2.8.1.tar.gz ] && [ `$CHECKSUM hadoop-2.8.1.tar.gz |awk '{print $4}'` = "def2211ee1561871794e1b0eec8cb628" ]; then
+if [ -s hadoop-2.8.1.tar.gz ] && [[ `$CHECKSUM hadoop-2.8.1.tar.gz |awk '{print $4}'` = "def2211ee1561871794e1b0eec8cb628" ]]; then
     echo "file exists and checksum passed"
 else
     echo "downloading hadoop-2.8.1 ..."
@@ -17,7 +17,7 @@ fi
 
 
 echo "checking jdk-8u152-linux-x64.rpm ..."
-if [ -s jdk-8u152-linux-x64.rpm ] && [ `$CHECKSUM jdk-8u152-linux-x64.rpm |awk '{print $4}'` = "b6979be30bdc4077dc93cd99134ad84d" ]; then
+if [ -s jdk-8u152-linux-x64.rpm ] && [[ `$CHECKSUM jdk-8u152-linux-x64.rpm |awk '{print $4}'` = "b6979be30bdc4077dc93cd99134ad84d" ]]; then
     echo "file exists and checksum passed"
 else
     echo "downloading jdk-8u152-linux-x64.rpm ..."
@@ -26,7 +26,7 @@ else
 fi
 
 echo "checking apache-storm-1.1.0.tar.gz ..."
-if [ -s apache-storm-1.1.0.tar.gz ] && [ `$CHECKSUM apache-storm-1.1.0.tar.gz |awk '{print $4}'` = "531294419a45ceb639db065c5b007bd4" ]; then
+if [ -s apache-storm-1.1.0.tar.gz ] && [[ `$CHECKSUM apache-storm-1.1.0.tar.gz |awk '{print $4}'` = "531294419a45ceb639db065c5b007bd4" ]]; then
     echo "file exists and checksum passed"
 else
     echo "downloading apache-storm-1.1.0.tar.gz ..."
@@ -35,7 +35,7 @@ else
 fi
 
 echo "checking  zookeeper-3.4.11.tar.gz..."
-if [ -s zookeeper-3.4.11.tar.gz ] && [ `$CHECKSUM zookeeper-3.4.11.tar.gz |awk '{print $4}'` = "55aec6196ed9fa4c451cb5ae4a1f42d8" ]; then
+if [ -s zookeeper-3.4.11.tar.gz ] && [[ `$CHECKSUM zookeeper-3.4.11.tar.gz |awk '{print $4}'` = "55aec6196ed9fa4c451cb5ae4a1f42d8" ]]; then
     echo "file exists and checksum passed"
 else
     echo "downloading zookeeper-3.4.11.tar.gz"
