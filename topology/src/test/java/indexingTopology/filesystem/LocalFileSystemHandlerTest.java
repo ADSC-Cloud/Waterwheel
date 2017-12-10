98d0ccc5d8c03847b642964ef082e09ea5606b9d
Bill Lin <billlin@BilldeMacBook-Pro.local>
2017-12-10 10:12:49 +0800

    commit

--- topology/src/test/java/indexingTopology/filesystem/LocalFileSystemHandlerTest.java
+++ topology/src/test/java/indexingTopology/filesystem/LocalFileSystemHandlerTest.java
@@ -19,31 +19,20 @@ public class LocalFileSystemHandlerTest {
         String str = "Hello World!";
         TopologyConfig config = new TopologyConfig();
         LocalFileSystemHandler localFileTest = new LocalFileSystemHandler(path, config);
-        localFileTest.createNewFile("","Test.md");
-        localFileTest.openFile("","Test.md");
+        localFileTest.createNewFile("","asd.txt");
+        localFileTest.openFile("","asd.txt");
         // Memchunk : memory chunk
         MemChunk memChunk1 = MemChunk.createNew(str.length());
-//        MemChunk memChunk2 = MemChunk.createNew(str2.length());
         Output output = new Output(TopologyConfig.DataChunkHeaderSectionSize);
         output.writeString(str);
         byte[] bytes = output.toBytes();
-//        output.clear();
-//        output.writeString(str2);
-//        byte[] bytes2 = output.toBytes();
         output.close();
-//        int flag = str1.length();
         memChunk1.write(bytes);
-//        memChunk2.write(bytes2);
-        localFileTest.writeToFileSystem(memChunk1,"","test.md");
-//        localFileTest.writeToFileSystem(memChunk2,"","test.md");
+        localFileTest.writeToFileSystem(memChunk1,"","asd.txt");
 
 
-        byte array1[] = new byte[str.length()+1];
-//        byte array2[] = new byte[str2.length()];
-        //memChunk.getAllocatedSize();
+        byte array1[] = new byte[100];
         localFileTest.readBytesFromFile(array1);
-//        localFileTest.readBytesFromFile(6,array2);
-//        int b = memChunk.write(array);
         Input input = new Input(array1);
         String result = input.readString();
         assertEquals("Hello World!",result);
@@ -54,13 +43,13 @@ public class LocalFileSystemHandlerTest {
         assertEquals("World!",result);
     }
 
-//    @After
-//    public void tearDown() {
-//        try {
-//            Runtime.getRuntime().exec("rm ./Test.md");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @After
+    public void tearDown() {
+        try {
+            Runtime.getRuntime().exec("rm ./asd.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
