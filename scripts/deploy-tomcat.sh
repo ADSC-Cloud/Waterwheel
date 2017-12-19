f33a78993e944ce3f1a7e5dc38dc98b4923f4c8f
Li Wang <wangli1426@gmail.com>
2017-12-19 15:11:57 +0800

    fixed the parse error in storm-slave.yaml

--- scripts/deploy-tomcat.sh
+++ scripts/deploy-tomcat.sh
@@ -9,17 +9,17 @@ fi
 
 
 if [ ! -s apache-tomcat-8.5.23.tar.gz ]; then
-    wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz
+    wget http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.24/bin/apache-tomcat-8.5.24.tar.gz
 else
-    echo "apache-tomcat-8.5.23.tar.gz is found."
+    echo "apache-tomcat-8.5.24.tar.gz is found."
 fi
 
-tar xf apache-tomcat-8.5.23.tar.gz
+tar xf apache-tomcat-8.5.24.tar.gz
 echo "tomcat is extracted from tar.gz file"
 
-cp waterwheel.war apache-tomcat-8.5.23/webapps/
+cp waterwheel.war apache-tomcat-8.5.24/webapps/
 echo "waterwheel.war is copied into tomcat../webapps/"
-apache-tomcat-8.5.23/bin/startup.sh
+apache-tomcat-8.5.24/bin/startup.sh
 
 echo "You can visit the web ui via http://localhost:8080/waterwheel."
 echo "Enjoy it!" 
